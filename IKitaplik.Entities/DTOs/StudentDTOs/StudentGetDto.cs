027d339ca04d4bfb0193f603d977ea19fe5cf98c
Hamza Irmak <hamzairmak46tr@gmail.com>
2025-08-02 23:16:52 +0300

    Öğrenciler için frontend hazırlandı

--- IKitaplik.Entities/DTOs/StudentDTOs/StudentGetDto.cs
+++ IKitaplik.Entities/DTOs/StudentDTOs/StudentGetDto.cs
@@ -8,7 +8,7 @@ public class StudentGetDto: IDto
     public string Class { get; set; }
     public string TelephoneNumber { get; set; }
     public string EMail { get; set; }
-    public string Point { get; set; }
+    public int Point { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
 } 
