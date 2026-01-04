d12403ebbe97b3b7c36b52b1e6bb392c3fc1ce4a
Hamza Irmak <hamzairmak46tr@gmail.com>
2026-01-04 21:33:18 +0300

    API ve dosya URL'leri canlı sunucuya güncellendi
    Uygulamanın varsayılan API ve dosya URL'leri canlı sunucu adresleriyle değiştirildi. Yerel sunucu adresleri yorum satırı haline getirildi. Artık uygulama canlı ortamda çalışacak şekilde yapılandırıldı.

--- IKitaplik.BlazorUI/Cosntant/Settings.cs
+++ IKitaplik.BlazorUI/Cosntant/Settings.cs
@@ -2,10 +2,10 @@
 {
     public static class Settings
     {
-        //public static string apiUrl = "https://kutuphane.api.hamzairmak.com.tr/api/";
-        //public static string fileUrl = "https://kutuphane.api.hamzairmak.com.tr/";
+        public static string apiUrl = "https://kutuphane.api.hamzairmak.com.tr/api/";
+        public static string fileUrl = "https://kutuphane.api.hamzairmak.com.tr/";
 
-        public static string apiUrl = "https://localhost:7228/api/";
-        public static string fileUrl = "https://localhost:7228/";
+        //public static string apiUrl = "https://localhost:7228/api/";
+        //public static string fileUrl = "https://localhost:7228/";
     }
 }
